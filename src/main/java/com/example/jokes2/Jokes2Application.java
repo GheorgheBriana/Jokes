ba340af64c6a00db1edae0d726c63a8877955034
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2026-03-11 12:19:45 +0200

    Initial commit

---
+++ src/main/java/com/example/jokes2/Jokes2Application.java
@@ -0,0 +1,13 @@
+package com.example.jokes2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jokes2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jokes2Application.class, args);
+    }
+
+}
