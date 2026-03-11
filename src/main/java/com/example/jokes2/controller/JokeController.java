41886265eb529adc158ea153734465b6ed134a8f
Gheorghe Briana-Maria <gheorghebrianamaria@gmail.com>
2026-03-11 12:56:56 +0200

    Modified getJokes() method in Controller

--- src/main/java/com/example/jokes2/controller/JokeController.java
+++ src/main/java/com/example/jokes2/controller/JokeController.java
@@ -3,6 +3,7 @@ package com.example.jokes2.controller;
 import com.example.jokes2.model.JokeCollection;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,12 +19,13 @@ public class JokeController {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping("/jokes")
+    @GetMapping("/jokes/{amount}")
     @ResponseBody
-    public List<JokeCollection.Joke> getJokes() {
+    public List<JokeCollection.Joke> getJokes(@PathVariable int amount) {
         String uri = "https://v2.jokeapi.dev/joke/any?amount={amount}";
-        ResponseEntity<JokeCollection> responseEntity = restTemplate.getForEntity(uri, JokeCollection.class, 2);
+        ResponseEntity<JokeCollection> responseEntity = restTemplate.getForEntity(uri, JokeCollection.class, amount);
         JokeCollection jokeCollection = responseEntity.getBody();
         return jokeCollection.getJokes();
     }
+
 }
